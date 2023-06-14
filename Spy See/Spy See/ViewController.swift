ab1e71ccca9b33e5fc61bcd34f829973b20c7dff
Trevor Yang <frank810903@gmail.com>
2023-06-14 23:59:09 +0800

    Login 3 test accounts

--- Spy See/Spy See/ViewController.swift
+++ Spy See/Spy See/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func check(_ sender: UIButton) {
+        
+        if let user = Auth.auth().currentUser {
+            print("\(user.uid) login")
+        } else {
+            print("not login")
+        }
+        
+    }
+    
+    @IBAction func logIn(_ sender: UIButton) {
+        
+        Auth.auth().signIn(withEmail: account.text ?? "", password: password.text ?? "") { result, error in
+             guard error == nil else {
+                 print(error?.localizedDescription ?? "")
+                return
+             }
+            print("\(self.account.text ?? "") log in")
+        }
+        
+    }
 }
 
