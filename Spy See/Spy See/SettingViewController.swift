2437a4b40d10fb0e84f4e308622457329c3ed4a8
Trevor Yang <frank810903@gmail.com>
2023-06-15 15:24:55 +0800

    Add prompt and player to room

--- Spy See/Spy See/SettingViewController.swift
+++ Spy See/Spy See/SettingViewController.swift
@@ -7,30 +7,33 @@
 
 import UIKit
 import FirebaseFirestore
+import FirebaseAuth
 import CryptoKit
 
 class SettingViewController: UIViewController {
     @IBOutlet weak var playerNumber: UITextField!
     @IBOutlet weak var spyNumber: UITextField!
     let dataBase = Firestore.firestore()
-    let promptArray: [String] = []
+    var promptArray: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
     @IBAction func createInvationCode(_ sender: UIButton) {
-        let room = dataBase.collection("Room")
-        let documentRef = room.document()
-        
+        let room = dataBase.collection("Rooms")
         let roomId = generateRoomId()
-        
+        let documentRef = room.document(roomId)
         let data: [String: Any] = [
-            "roomId": roomId,
-            
-            
-        
-        
+            "prompts": generatePromptArray(),
+            "player": String(describing: Auth.auth().currentUser)
         ]
+        documentRef.setData(data) { error in
+            if let error = error {
+                print("Error adding document: \(error)")
+            } else {
+                print("Document added successfully")
+            }
+        }
     }
     func generateRoomId() -> String {
         let inviteCodeLength = 4
@@ -41,7 +44,15 @@ class SettingViewController: UIViewController {
         return roomId
     }
     func generatePromptArray() -> [String] {
+        promptArray = []
         let choosedPrompt = prompt.randomElement()
-        
+        for _ in 0...(Int(playerNumber.text ?? "") ?? 0) - (Int(spyNumber.text ?? "") ?? 0) - 1 {
+            promptArray.append(choosedPrompt?.0 ?? "")
+        }
+        for _ in 0...(Int(spyNumber.text ?? "") ?? 0) - 1 {
+            promptArray.append(choosedPrompt?.1 ?? "")
+        }
+        promptArray.shuffle()
+        return promptArray
     }
 }
