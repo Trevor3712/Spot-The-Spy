50fcdb7c87c63f0fcbcb25a139fb2c9c4e6c2c17
Trevor Yang <frank810903@gmail.com>
2023-06-18 14:30:00 +0800

    Modify pop voted player style

--- Spy See/Spy See/KillViewController.swift
+++ Spy See/Spy See/KillViewController.swift
@@ -21,19 +21,21 @@ class KillViewController: UIViewController {
         let room = dataBase.collection("Rooms")
         let roomId = UserDefaults.standard.string(forKey: "roomId") ?? ""
         let documentRef = room.document(roomId)
-        documentRef.addSnapshotListener { (documentSnapshot, error) in
-            if let error = error {
-                print(error)
-                return
-            }
-            guard let data = documentSnapshot?.data() else {
-                print("No data available")
-                return
-            }
-            if let voted = data["voted"] as? [[String: String]] {
-                self.votedArray = voted
-                print(self.votedArray)
-                self.killWhchPlayer()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+            documentRef.getDocument { (documentSnapshot, error) in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                guard let data = documentSnapshot?.data() else {
+                    print("No data available")
+                    return
+                }
+                if let voted = data["voted"] as? [[String: String]] {
+                    self.votedArray = voted
+                    print(self.votedArray)
+                    self.killWhchPlayer()
+                }
             }
         }
     }
@@ -49,10 +51,10 @@ class KillViewController: UIViewController {
 
         // 查找出现次数最多的值
         if let (mostFrequentValue, _) = voteCount.max(by: { $0.value < $1.value }) {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
                 self.waitLabel.text = ""
                 self.votedLabel.text = "\(mostFrequentValue)被殺死了！"
-            }
+//            }
         }
     }
 }
