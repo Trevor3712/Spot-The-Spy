c49552b14ff18e1edca08d531b85cd8df402b231
Trevor Yang <frank810903@gmail.com>
2023-06-18 21:06:25 +0800

    Add end round logic and remove voted player

--- Spy See/Spy See/KillViewController.swift
+++ Spy See/Spy See/KillViewController.swift
@@ -12,9 +12,12 @@ class KillViewController: UIViewController {
     @IBOutlet weak var votedLabel: UILabel!
     @IBOutlet weak var waitLabel: UILabel!
     @IBOutlet weak var identityLabel: UILabel!
+    @IBOutlet weak var nextRoundButton: UIButton!
     let dataBase = Firestore.firestore()
     var votedArray: [[String: String]] = []
-    var identitesArray: [String] = []
+    var identitiesArray: [String] = []
+    var arrayIndex: Int?
+    var playersArray: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         loadVotedPlayers()
@@ -38,8 +41,8 @@ class KillViewController: UIViewController {
                     print(self.votedArray)
                 }
                 if let identities = data["identities"] as? [String] {
-                    self.identitesArray = identities
-                    print(self.identitesArray)
+                    self.identitiesArray = identities
+                    print(self.identitiesArray)
                 }
                 self.killWhchPlayer()
             }
@@ -57,11 +60,48 @@ class KillViewController: UIViewController {
         // 查找出現次數最多的值
         if let (mostFrequentValue, _) = voteCount.max(by: { $0.value < $1.value }) {
             let players = UserDefaults.standard.stringArray(forKey: "playersArray")
+            playersArray = players ?? [""]
             if let index = players?.firstIndex(of: mostFrequentValue) {
+                arrayIndex = index
                 print("mostFrequentValue: \(mostFrequentValue), index: \(index)")
                 self.waitLabel.text = ""
                 self.votedLabel.text = "\(mostFrequentValue)被殺死了！"
-                self.identityLabel.text = "他的身份是\(identitesArray[index])"
+                self.identityLabel.text = "他的身份是\(identitiesArray[index])"
+            }
+        }
+    }
+    @IBAction func nextRound(_ sender: UIButton) {
+        self.playersArray.remove(at: arrayIndex ?? 0)
+        self.identitiesArray.remove(at: arrayIndex ?? 0)
+        self.votedArray.removeAll()
+        self.updateData()
+        let countCivilian = identitiesArray.filter { $0 == "平民" }.count
+        let countSpy = identitiesArray.filter { $0 == "臥底" }.count
+        if countSpy == 0 {
+            // 去平民獲勝頁面
+            print("平民獲勝！")
+        } else if countSpy >= countCivilian {
+            // 去臥底獲勝頁面
+            print("臥底獲勝！")
+        } else {
+            // 回到發言頁
+            print("繼續下一輪")
+        }
+    }
+    func updateData() {
+        let room = dataBase.collection("Rooms")
+        let roomId = UserDefaults.standard.string(forKey: "roomId") ?? ""
+        let documentRef = room.document(roomId)
+        let data: [String: Any] = [
+            "player": playersArray,
+            "identities": identitiesArray,
+            "voted": votedArray
+        ]
+        documentRef.updateData(data) { error in
+            if let error = error {
+                print("Error updating document: \(error)")
+            } else {
+                print("Document updated successfully")
             }
         }
     }
