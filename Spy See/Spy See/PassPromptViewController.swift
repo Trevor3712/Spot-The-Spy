494a0f74db7ecb62fd1abfdb1f183b0dc997a31d
Trevor Yang <frank810903@gmail.com>
2023-06-22 23:29:05 +0800

    Segue to speak page after all players ready

--- Spy See/Spy See/PassPromptViewController.swift
+++ Spy See/Spy See/PassPromptViewController.swift
@@ -6,22 +6,95 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class PassPromptViewController: UIViewController {
     @IBOutlet weak var promptLabel: UILabel!
     var playerPrompt: String?
+    let dataBase = Firestore.firestore()
+    var readyPlayers: [String] = []
+    var playerNumber: Int?
     override func viewDidLoad() {
         super.viewDidLoad()
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             let playerPrompt = UserDefaults.standard.string(forKey: "playerPrompt")
             let hostPrompt = UserDefaults.standard.string(forKey: "hostPrompt")
-            print(playerPrompt)
-            print(hostPrompt)
             if playerPrompt != nil {
                 self.promptLabel.text = playerPrompt
             } else {
                 self.promptLabel.text = hostPrompt
             }
+        }
+        loadReadyPlayers()
+    }
+    @IBAction func readyButtonPressed(_ sender: UIButton) {
+        let room = self.dataBase.collection("Rooms")
+        let roomId = UserDefaults.standard.string(forKey: "roomId") ?? ""
+        let documentRef = room.document(roomId)
+        guard let email = Auth.auth().currentUser?.email else {
+            print("Email is missing")
+            return
+        }
+        documentRef.getDocument { (documentSnapshot, error) in
+            if let error = error {
+                print("Error retrieving document: \(error)")
+                return
+            }
+            guard let document = documentSnapshot, document.exists else {
+                print("Document does not exist")
+                return
+            }
+            var playersReady = document.data()?["playersReady"] as? [String] ?? []
+            if !playersReady.contains(email) {
+                playersReady.append(email)
+            }
+            let data: [String: Any] = [
+                "playersReady": playersReady
+            ]
+            documentRef.updateData(data) { error in
+                if let error = error {
+                    print("Error updating document: \(error)")
+                } else {
+                    print("Document updated successfully")
+                }
+            }
+        }
+    }
+    func loadReadyPlayers() {
+        let room = self.dataBase.collection("Rooms")
+        let roomId = UserDefaults.standard.string(forKey: "roomId") ?? ""
+        let documentRef = room.document(roomId)
+        var existingPlayers: Set<String> = Set(self.readyPlayers)
+        documentRef.addSnapshotListener { (documentSnapshot, error) in
+            DispatchQueue.main.async {
+                guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error)")
+                    return
+                }
+                if let players = document.data()?["player"] as? [String] {
+                    self.playerNumber = players.count
+                }
+                let playersReady = document.data()?["playersReady"] as? [String] ?? []
+                self.readyPlayers = []
+                let newPlayers = playersReady.filter { !existingPlayers.contains($0) }
+                self.readyPlayers.append(contentsOf: newPlayers)
+                if self.isAllPlayersReady() {
+                    self.performSegue(withIdentifier: "PromptToSpeak", sender: self)
+                }
+            }
+        }
+    }
+    func isAllPlayersReady() -> Bool {
+        print("playerNumber\(Int(playerNumber ?? 0))")
+        print("readyPlayers:\(self.readyPlayers.count)")
+        return self.readyPlayers.count == playerNumber
+    }
+}
+
+extension PassPromptViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PromptToSpeak" {
         }
     }
 }
