c3ba0714fc6c3cfa5e1ceaf69567c7d50570c81c
Trevor Yang <frank810903@gmail.com>
2023-06-23 18:50:20 +0800

    Add color extension code

---
+++ Spy See/Spy See/Extensions/UIColor+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+Extension.swift
+//  Spy See
+//
+//  Created by 楊哲維 on 2023/6/23.
+//
+
+import UIKit
+
+private enum Color: String {
+    case B1
+    case B2
+    case B3
+    case Y
+    case R
+}
+extension UIColor {
+    static let B1 = Color(.B1)
+    static let B2 = Color(.B2)
+    static let B3 = Color(.B3)
+    static let Y = Color(.Y)
+    static let R = Color(.R)
+    
+    private static func Color(_ color: Color) -> UIColor? {
+        return UIColor(named: color.rawValue)
+    }
+}
