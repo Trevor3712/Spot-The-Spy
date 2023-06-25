3fc22de6dbe558ca2255bbd321e81e0aecba63c6
Trevor Yang <frank810903@gmail.com>
2023-06-25 18:40:46 +0800

    Finish invite page UI

--- Spy See/Spy See/InviteViewController.swift
+++ Spy See/Spy See/InviteViewController.swift
@@ -8,12 +8,89 @@
 import UIKit
 
 class InviteViewController: BaseViewController {
-    @IBOutlet weak var inviteLabel: UILabel!
+    lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.attributedText = UIFont.fontStyle(
+            font: .semibold,
+            title: "邀請碼",
+            size: 20,
+            textColor: .B2 ?? .black,
+            letterSpacing: 10)
+        return titleLabel
+    }()
+    lazy var invitationLabel: UILabel = {
+        let invitationLabel = UILabel()
+        invitationLabel.attributedText = UIFont.fontStyle(
+            font: .semibold,
+            title: roomId ?? "",
+            size: 50,
+            textColor: .B2 ?? .black,
+            letterSpacing: 20)
+        invitationLabel.backgroundColor = .white
+        invitationLabel.layer.borderWidth = 1
+        invitationLabel.layer.borderColor = UIColor.B1?.cgColor
+        invitationLabel.layer.cornerRadius = 20
+        invitationLabel.clipsToBounds = true
+        invitationLabel.textAlignment = .center
+        return invitationLabel
+    }()
+    lazy var copyButton: BaseButton = {
+        let copyButton = BaseButton()
+        copyButton.setAttributedTitle(UIFont.fontStyle(
+            font: .semibold,
+            title: "複製邀請碼",
+            size: 20,
+            textColor: .B2 ?? .black,
+            letterSpacing: 5), for: .normal)
+        copyButton.titleLabel?.textAlignment = .center
+        copyButton.addTarget(self, action: #selector(copyButtonPressed), for: .touchUpInside)
+        return copyButton
+    }()
+    lazy var readyButton: BaseButton = {
+        let readyButton = BaseButton()
+        readyButton.setAttributedTitle(UIFont.fontStyle(
+            font: .semibold,
+            title: "進入房間",
+            size: 20,
+            textColor: .B2 ?? .black,
+            letterSpacing: 5), for: .normal)
+        readyButton.titleLabel?.textAlignment = .center
+        readyButton.addTarget(self, action: #selector(readyButtonPressed), for: .touchUpInside)
+        return readyButton
+    }()
+    let roomId = UserDefaults.standard.string(forKey: "roomId")
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let roomId = UserDefaults.standard.string(forKey: "roomId")
-//        inviteLabel.text = roomId
+        [titleLabel, invitationLabel, copyButton, readyButton].forEach { view.addSubview($0) }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(150)
+            make.centerX.equalTo(view)
+        }
+        invitationLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel).offset(100)
+            make.centerX.equalTo(view)
+            make.width.equalTo(250)
+            make.height.height.equalTo(80)
+        }
+        copyButton.snp.makeConstraints { make in
+            make.top.equalTo(invitationLabel.snp.bottom).offset(50)
+            make.centerX.equalTo(view)
+            make.width.equalTo(150)
+            make.height.equalTo(40)
+        }
+        readyButton.snp.makeConstraints { make in
+            make.top.equalTo(copyButton.snp.bottom).offset(130)
+            make.centerX.equalTo(view)
+            make.width.equalTo(150)
+            make.height.equalTo(40)
+        }
     }
-    @IBAction func joinRoom(_ sender: UIButton) {
+    @objc func copyButtonPressed() {
+        let copyText = invitationLabel.text
+        UIPasteboard.general.string = "我想邀請你來玩誰是臥底，邀請碼：\(copyText ?? "")"
+    }
+    @objc func readyButtonPressed() {
+        let waitingVC = WaitingViewController()
+        navigationController?.pushViewController(waitingVC, animated: true)
     }
 }
