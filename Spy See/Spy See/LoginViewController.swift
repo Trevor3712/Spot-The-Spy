fa83a7faf22a8ccf75441c60a1ed4685bf6c44d6
Trevor Yang <frank810903@gmail.com>
2023-06-25 12:46:29 +0800

    Finish login page UI

--- Spy See/Spy See/LoginViewController.swift
+++ Spy See/Spy See/LoginViewController.swift
@@ -68,16 +68,36 @@ class LoginViewController: BaseViewController {
     lazy var accountTextFileld: BaseTextField = {
         let accountTextFileld = BaseTextField()
         accountTextFileld.placeholder = "請輸入帳號"
+        accountTextFileld.text = "1@1.com"
         return accountTextFileld
     }()
     lazy var passwordTextFileld: BaseTextField = {
         let passwordTextFileld = BaseTextField()
         passwordTextFileld.placeholder = "請輸入密碼"
+        passwordTextFileld.text = "123456"
         return passwordTextFileld
+    }()
+    lazy var loginButton: BaseButton = {
+        let loginButton = BaseButton()
+        loginButton.setAttributedTitle(UIFont.fontStyle(font: .regular, title: "登入", size: 20, textColor: .B2 ?? .black, letterSpacing: 3), for: .normal)
+        loginButton.titleLabel?.textAlignment = .center
+        loginButton.addTarget(self, action: #selector(logInButtonPressed), for: .touchUpInside)
+        return loginButton
+    }()
+    lazy var signupButton: BaseButton = {
+        let signupButton = BaseButton()
+        signupButton.setAttributedTitle(UIFont.fontStyle(font: .regular, title: "註冊", size: 20, textColor: .B2 ?? .black, letterSpacing: 3), for: .normal)
+        signupButton.titleLabel?.textAlignment = .center
+        return signupButton
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        [logoImage, labelCN1, labelCN2, labelEN1, labelEN2, labelEN3, accountTextFileld, passwordTextFileld].forEach { view.addSubview($0) }
+        [
+            logoImage,
+            labelCN1, labelCN2, labelEN1, labelEN2, labelEN3,
+            accountTextFileld, passwordTextFileld,
+            loginButton, signupButton
+        ].forEach { view.addSubview($0) }
         logoImage.snp.makeConstraints { make in
             make.top.equalTo(view).offset(120)
             make.centerX.equalTo(view)
@@ -104,28 +124,43 @@ class LoginViewController: BaseViewController {
             make.top.equalTo(labelCN2.snp.bottom)
             make.right.equalTo(view).offset(-90)
         }
-//        accountTextFileld.snp.makeConstraints { make in
-//            make.top.equalTo(view).offset(400)
-//            make.centerX.equalTo(view)
-//            make.width.equalTo(200)
-//            make.height.equalTo(40)
-//        }
-//        passwordTextFileld.snp.makeConstraints { make in
-//            make.top.equalTo(accountTextFileld.snp.bottom).offset(40)
-//            make.centerX.equalTo(view)
-//            make.width.equalTo(200)
-//            make.height.equalTo(40)
-//        }
+        accountTextFileld.snp.makeConstraints { make in
+            make.top.equalTo(labelEN3.snp.bottom).offset(12)
+            make.centerX.equalTo(view)
+            make.width.equalTo(260)
+            make.height.equalTo(40)
+        }
+        passwordTextFileld.snp.makeConstraints { make in
+            make.top.equalTo(accountTextFileld.snp.bottom).offset(30)
+            make.centerX.equalTo(view)
+            make.width.equalTo(260)
+            make.height.equalTo(40)
+        }
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextFileld.snp.bottom).offset(30)
+            make.left.equalTo(passwordTextFileld)
+            make.width.equalTo(115)
+            make.height.equalTo(40)
+        }
+        signupButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextFileld.snp.bottom).offset(30)
+            make.right.equalTo(passwordTextFileld)
+            make.width.equalTo(115)
+            make.height.equalTo(40)
+        }
     }
-//    @IBAction func logIn(_ sender: UIButton) {
-//        Auth.auth().signIn(withEmail: account.text ?? "", password: password.text ?? "") { _, error in
-//            guard error == nil else {
-//                print(error?.localizedDescription ?? "")
-//                return
-//            }
-//            print("\(self.account.text ?? "") log in")
-//            let lobbyVC = LobbyViewController()
-//            self.navigationController?.pushViewController(lobbyVC, animated: true)
-//        }
-//    }
+    @objc func logInButtonPressed(_ sender: UIButton) {
+        Auth.auth().signIn(
+            withEmail: accountTextFileld.text ?? "",
+            password: passwordTextFileld.text ?? "")
+        { _, error in
+            guard error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            print("\(self.accountTextFileld.text ?? "") log in")
+            let lobbyVC = LobbyViewController()
+            self.navigationController?.pushViewController(lobbyVC, animated: true)
+        }
+    }
 }
